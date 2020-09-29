db24ca818addd960fd226c271270c15ce1ffd97f
aditya Kharade <adityakharade51@gmail.com>
2020-09-29 19:34:47 +0530

    Added: added fuctionality to validate name

--- src/Registration.java
+++ src/Registration.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Registration {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Registration user = new Registration();
+		System.out.println("Enter the first name");
+		String firstName = sc.nextLine();
+		System.out.println(firstName + " is " +user.validateInput(firstName));
+		
+		sc.close();
+	}
+	private String validateInput(String name) {
+		String expression = "(^[A-Z]{1})[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(expression);
+		Matcher match = pattern.matcher(name);
+		if(match.find()) {
+			return "valid";
+		}
+		return "invalid";
+	}
 }
