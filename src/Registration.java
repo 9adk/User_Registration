b9bdeec434afb47928e9e6bf1ec52ed382164a02
aditya Kharade <adityakharade51@gmail.com>
2020-09-29 23:28:23 +0530

    Added : added functionality for validating email

--- src/Registration.java
+++ src/Registration.java
@@ -14,9 +14,14 @@ public class Registration {
 		String lastName = sc.nextLine();
 		System.out.println("Enter the Email Id");
 		String Email = sc.nextLine();
+		System.out.println("Enter the Phone Number");
+		String Phone = sc.nextLine();
+		
 		System.out.println(firstName + " is " +user.validateInput(firstName)); //Validating firstName
 		System.out.println(lastName + " is " +user.validateInput(lastName)); //Validating lastName
-		System.out.println(Email + " is " +user.validateEmail(Email));
+		System.out.println(Email + " is " +user.validateEmail(Email));	// Validating Email
+		System.out.println(Phone+ " is " +user.validateNumber(Phone));	// Validating Number
+		
 		sc.close();
 	}
 	private String validateInput(String name) {
@@ -29,7 +34,7 @@ public class Registration {
 		return "invalid";
 	}
 	private String validateEmail(String Email) {
-		String expression = "(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{2,})$"; // Pattern for Email
+		String expression = "(abc?[.][A-Za-z]*@bl[.]co[.][A-Za-z]{2,})$"; // Pattern for Email
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(Email);
 		if(match.find()) {
@@ -37,6 +42,17 @@ public class Registration {
 		}
 		return "invalid";
 	}
+	private String validateNumber(String number) {
+		String expression = "^[0-9]{2}?[ ][0-9]{4,13}";	//Pattern for Number
+		Pattern pattern = Pattern.compile(expression);
+		Matcher match = pattern.matcher(number);
+		if(match.find()) {
+			return "valid";
+		}
+		return "invalid";
+		
+	}
+	
 	
 	
 	
