bab5d8b9ce46dec8258e1874e10f596e725a93d1
aditya Kharade <adityakharade51@gmail.com>
2020-09-29 19:42:01 +0530

    Modified : validating last name using existing functionality

--- src/Registration.java
+++ src/Registration.java
@@ -5,15 +5,20 @@ import java.util.regex.Pattern;
 public class Registration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		Registration user = new Registration();
+		
 		System.out.println("Enter the first name");
 		String firstName = sc.nextLine();
-		System.out.println(firstName + " is " +user.validateInput(firstName));
+		System.out.println("Enter the last name");
+		String lastName = sc.nextLine();
 		
+		System.out.println(firstName + " is " +user.validateInput(firstName)); //Validating firstName
+		System.out.println(lastName + " is " +user.validateInput(lastName)); //Validating lastName
 		sc.close();
 	}
 	private String validateInput(String name) {
-		String expression = "(^[A-Z]{1})[a-z]{2,}$";
+		String expression = "(^[A-Z]{1})[a-z]{2,}$"; // Pattern for Names
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(name);
 		if(match.find()) {
